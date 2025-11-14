4920892354f2ac3daec78bdff74bad5659619eec
iwahyuendriarto@gmail.com <ikhsanwahyue_iwahyuendriarto@gmail.com>
2025-11-14 17:36:17 +0700

    sudah ya

--- views/login.php
+++ views/login.php
@@ -3,47 +3,43 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Login Azizi.io</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
       body {
-        background-color: #0d6efd;
+        background-color: #2d3250;
         height: 100vh;
         display: flex;
         align-items: center;
         justify-content: center;
       }
       .login-card {
-        background-color: #fff;
+        margin-left: 500px;
+        background-color: #424769;
         padding: 2rem;
         border-radius: 10px;
         box-shadow: 0 0 30px rgba(0,0,0,0.1);
         width: 100%;
         max-width: 500px;
-        margin-left: 500px;
       }
       .form-label {
         font-weight: 500;
+        color: #fff;
+      }
+      .btn {
+        background-color: #f9b17a;
       }
     </style>
   </head>
   <body>
     <div class="login-card">
-      <img src="asset/logo-ikhsan.png" alt="Logo Buku Ikhsan" class="mb-3 d-block mx-auto" style="width: 80px;">
-      <h4 class="fw-bold mb-1 text-center">Welcome to</h4>
-      <p class="text-muted mb-4">Login to Azizi.io</p>
-
-        <div class="mb-3">
-            <label for="Google" class="form-label">Login dengan Google</label>
-          <input type="onclick" class="form-control" id="email" name="email" placeholder="Continue With Google" required>
-        </div>
-
-        <div class="text-muted mb-3 text-center">atau login dengan Manual</div>
-
+      <img src="../public/asset/LogoAziziz.png" alt="Logo Azizi.io" class="mb-3 d-block mx-auto" style="width: 80px; color: white; ">
+      <h4 class="fw-bold mb-5 text-center" style="color: white;">Masuk ke Buku Digital By Azizi.io</h4>
+      
       <form action="proses_login.php" method="POST">
         <div class="mb-3">
-          <label for="email" class="form-label">No. WhatsApp atau Email</label>
-          <input type="text" class="form-control" id="email" name="email" placeholder="Masukan No. WhatsApp atau Email" required>
+          <label for="email" class="form-label">Email</label>
+          <input type="text" class="form-control" id="email" name="email" placeholder="Masukan Email Anda" required>
         </div>
 
         <div class="mb-3">
@@ -52,16 +48,13 @@
         </div>
 
         <div class="text-end mb-3">
-            <a href="" class="text-decoration-none small">Lupa Kata Sandi?</a>
+          <a href="#" class="text-decoration-none small text-light">Lupa Kata Sandi?</a>
         </div>
 
         <button type="submit" class="btn btn-primary w-100">Lanjutkan</button>
       </form>
 
-      <div class="text-center mt-3">
-        <a href="index.php" class="text-decoration-none">Kembali ke Beranda</a>
-      </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
